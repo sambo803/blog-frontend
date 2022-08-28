58b8fcf2612d956ae4ef77ad0d1602f015416d41
samuel <sbacon803@gmail.com>
2022-08-28 10:40:10 -0600

    add signup

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Signup from "../views/SignupView.vue";
+import LogoutView from "../views/LogoutView.vue";
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: Signup
+  },
+  {
+    path: "/logout",
+    name: "logout",
+    component: LogoutView
   },
   {
     path: '/about',
